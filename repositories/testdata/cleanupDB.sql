853e89c51b6dad6727679928261895c5d918be35
“daryuun5139” <118511364+daryuun5139@users.noreply.github.com>
2024-04-16 17:38:47 +0900

    chapter4 almost done

---
+++ repositories/testdata/cleanupDB.sql
@@ -0,0 +1,5 @@
+-- テスト実行後にデータを消す
+
+drop table if exists comments;
+
+drop table if exists articles;
---
+++ repositories/testdata/data.go
@@ -0,0 +1,35 @@
+// テスト実行時に使用するデータ
+
+package testdata
+
+import "github.com/daryuun5139/go_blog_app/models"
+
+var ArticleTestData = []models.Article{
+	models.Article{
+		ID:       1,
+		Title:    "firstPost",
+		Contents: "This is my first blog",
+		UserName: "saki",
+		NiceNum:  2,
+	},
+	models.Article{
+		ID:       2,
+		Title:    "2nd",
+		Contents: "Second blog post",
+		UserName: "saki",
+		NiceNum:  4,
+	},
+}
+
+var CommentTestData = []models.Comment{
+	models.Comment{
+		CommentID: 1,
+		ArticleID: 1,
+		Message:   "1st comment yeah",
+	},
+	models.Comment{
+		CommentID: 2,
+		ArticleID: 1,
+		Message:   "welcome",
+	},
+}
